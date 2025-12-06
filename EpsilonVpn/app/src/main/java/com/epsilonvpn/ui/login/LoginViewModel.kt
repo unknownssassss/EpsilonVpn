4acb9d593e6efa1afe87e4a82bfa509ede0b4ffc
unknownssassss <mahdi.khaleghi1234@gmail.com>
2025-12-06 15:33:59 +0330

    Create LoginViewModel.kt

---
+++ EpsilonVpn/app/src/main/java/com/epsilonvpn/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.epsilonvpn.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // برای آینده، اگر بخواهیم LiveData یا وضعیت ورود را مدیریت کنیم
+}
